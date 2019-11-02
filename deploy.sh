04c96efad01fbdebd0dc36b93861c3d2c984be49
Jason Weatherly <jamesthebard@gmail.com>
2019-11-02 14:41:19 -0400

    Testing deployments.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+SOURCE_IMAGE="${DOCKER_REPO}"
+TARGET_IMAGE="${AWS_ECR_REPO}/${DOCKER_REPO}"
+TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
+TARGET_IMAGE_TAGGED="${TARGET_IMAGE}:${TRAVIS_TAG}"
+
+aws configure set default.region ${AWS_REGION}
+$(aws ecr get-login --no-include-email)
+
+docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
+docker push ${TARGET_IMAGE_LATEST}
+
+docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_TAGGED}
+docker push ${TARGET_IMAGE_TAGGED}
