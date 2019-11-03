569d5213d5c824dee56a518ceb552865cda8e872
Jason Weatherly <jamesthebard@gmail.com>
2019-11-02 21:32:50 -0400

    Removed AWS pushes, moved to Docker Hub

--- ecr_push.sh
+++ ecr_push.sh
@@ -16,11 +16,11 @@ aws configure set default.region ${AWS_REGION}
 $(aws ecr get-login --no-include-email)
 
 # AWS ECR
-docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
-docker push ${TARGET_IMAGE_LATEST}
+#docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
+#docker push ${TARGET_IMAGE_LATEST}
 
-docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_TAGGED}
-docker push ${TARGET_IMAGE_TAGGED}
+#docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_TAGGED}
+#docker push ${TARGET_IMAGE_TAGGED}
 
 # Docker Hub
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
